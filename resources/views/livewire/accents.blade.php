065cc4f37b597312f7e65a22a03b3b6f7ab64f44
Martin Vandersteen <vandersteenmartin@gmail.com>
2022-01-29 14:55:05 +0100

    js

--- resources/views/livewire/accents.blade.php
+++ resources/views/livewire/accents.blade.php
@@ -1,6 +1,21 @@
 <div> 
-  <div>{{ $string }}</div>
-  <div>{{ $text }}</div>
+  <div>
+    <div>{{ $string }}</div>
+    <div>{{ $text }}</div>
+    <div>
+      <section>
+        {{ $text }}
+      </section>
+    </div>
+  </div>
   <div>{{ $number }}</div>
   <button wire:click="addNumber">Add number</button>
+
+  <script>
+    let object = {
+      'item_name': '{{ $string }}',
+      'item_id': '{{ $text }}',
+      'item_id': '{{ $number }}',
+    };
+</script>
 </div>
